7fada12ca7cbcc0260b36cec3b2db5d7364fc12b
netorapg <renatoplatz11@outlook.com>
2024-08-05 18:50:13 -0300

    fix issue

---
+++ wall/Wall.cpp
@@ -0,0 +1,9 @@
+#include "Wall.h"
+
+Wall::Wall(float x, float y, float width, float height) : Object(x, y, width, height) {}
+
+void Wall::render(SDL_Renderer *renderer) {
+    SDL_Rect fillRect = {static_cast<int>(mPosX), static_cast<int>(mPosY), static_cast<int>(mWidth), static_cast<int>(mHeight)};
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00); // White color for walls
+    SDL_RenderFillRect(renderer, &fillRect);
+}
