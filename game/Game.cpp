2bc1420ccd5407205b609b24aa8b4474a6d6ff2c
netorapg <renatoplatz11@outlook.com>
2024-09-10 15:03:21 -0300

    problema resolvido

--- game/Game.cpp
+++ game/Game.cpp
@@ -1,58 +1,74 @@
 #include "Game.h"
+
 #include "../config.h"
+
 #include <iostream>
 
-Game::Game(SDL_Window* window, SDL_Renderer* renderer)
-    : mWindow(window), mRenderer(renderer), mQuit(false), 
-      mPlayer(25, 5, mPlatforms, mSolidPlatforms, mWalls) {
-    std::cout << "Game constructor called" << std::endl;
+Game::Game( SDL_Window *window, SDL_Renderer *renderer )
+    : mWindow( window ), mRenderer( renderer ), mQuit( false ),
+      mPlayer( 25, 5, mPlatforms, mSolidPlatforms, mWalls )
+{
+  std::cout << "Game constructor called" << std::endl;
 }
 
-void Game::handleEvents() {
-    std::cout << "Handling events" << std::endl;
-    SDL_Event e;
-    while (SDL_PollEvent(&e) != 0) {
-        std::cout << "Event polled: " << e.type << std::endl;
-        if (e.type == SDL_QUIT) {
-            std::cout << "SDL_QUIT event received" << std::endl;
-            mQuit = true; // Atualize mQuit para true quando o evento SDL_QUIT for recebido
-        }
-        mPlayer.handleEvent(e);
+void Game::handleEvents()
+{
+  std::cout << "Handling events" << std::endl;
+  SDL_Event e;
+  while( SDL_PollEvent( &e ) != 0 )
+  {
+    std::cout << "Event polled: " << e.type << std::endl;
+    if( e.type == SDL_QUIT )
+    {
+      std::cout << "SDL_QUIT event received" << std::endl;
+      mQuit = true;   
     }
+    mPlayer.handleEvent( e );
+    std::cout << "Adding platforms" << std::endl;
+    mPlatforms.push_back( Platform( 10, SCREEN_HEIGHT - 150, 200, 20 ) );
+    mSolidPlatforms.push_back(
+      SolidPlatform( 100, SCREEN_HEIGHT - 150, 570, 20 ) );
+    mWalls.push_back( Wall( 200, SCREEN_HEIGHT - 250, 20, 100 ) );
+    mSolidPlatforms.push_back(
+      SolidPlatform( 200, SCREEN_HEIGHT - 260, 20, 10 ) );
+
+    mWalls.push_back( Wall( 0, 0, 20, 800 ) );
+    mWalls.push_back( Wall( 620, 0, 20, 800 ) );
+  }
 }
 
-void Game::update() {
-    std::cout << "Updating game state" << std::endl;
-    mPlayer.move();
+void Game::update()
+{
+  std::cout << "Updating game state" << std::endl;
+  mPlayer.move();
 }
 
-void Game::render() {
-    std::cout << "Rendering game" << std::endl;
-    SDL_SetRenderDrawColor(mRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-    SDL_RenderClear(mRenderer);
+void Game::render()
+{
+  std::cout << "Rendering game" << std::endl;
+  SDL_SetRenderDrawColor( mRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+  SDL_RenderClear( mRenderer );
 
-    std::cout << "Rendering platforms" << std::endl;
-    for (auto& platform : mPlatforms) {
-        platform.render(mRenderer);
-    }
+  std::cout << "Rendering platforms" << std::endl;
+  for( auto &platform : mPlatforms )
+    platform.render( mRenderer );
 
-    std::cout << "Rendering solid platforms" << std::endl;
-    for (auto& solidPlatform : mSolidPlatforms) {
-        solidPlatform.render(mRenderer);
-    }
+  std::cout << "Rendering solid platforms" << std::endl;
+  for( auto &solidPlatform : mSolidPlatforms )
+    solidPlatform.render( mRenderer );
 
-    std::cout << "Rendering walls" << std::endl;
-    for (auto &wall : mWalls) {
-        wall.render(mRenderer);
-    }
+  std::cout << "Rendering walls" << std::endl;
+  for( auto &wall : mWalls )
+    wall.render( mRenderer );
 
-    std::cout << "Rendering player" << std::endl;
-    mPlayer.render(mRenderer);
+  std::cout << "Rendering player" << std::endl;
+  mPlayer.render( mRenderer );
 
-    SDL_RenderPresent(mRenderer);
-    std::cout << "Render complete" << std::endl;
+  SDL_RenderPresent( mRenderer );
+  std::cout << "Render complete" << std::endl;
 }
 
-bool Game::isQuit() const {
-    return mQuit;
+bool Game::isQuit() const
+{
+  return mQuit;
 }
