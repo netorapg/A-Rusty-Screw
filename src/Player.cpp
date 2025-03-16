e3f8e8ea4b7a4312df4db67d2352271c82b700f0
netorag <renatoplatz11@outlook.com>
2025-03-16 14:35:18 -0300

    o sistema de animação esta funcionando até que bem, agora estou realizando alguns testes

--- src/Player.cpp
+++ src/Player.cpp
@@ -4,23 +4,50 @@
 namespace BRTC {
 
 Player::Player(Vector position, SDL_Renderer* renderer)
-    : DynamicObject(position, Vector(25, 26)),
+    : DynamicObject(position, Vector(40, 48)),
       mFacingRight(true),
-      mSprite(renderer, "../assets/bezourinha_correndo.png") 
+      mSprite(renderer, "../assets/bezourinha_sprites.png") 
 {
     // Configuração das animações
     Animation runAnim;
-    runAnim.addFrame({{0,   0, 25, 26}, 0.1f, {0, 0}});
-    runAnim.addFrame({{25,  0, 25, 26}, 0.1f, {0, 0}});
-    runAnim.addFrame({{50, 0, 25, 26}, 0.1f, {0, 0}});
+    runAnim.addFrame({{0, 204, 50, 48}, 5.5f, {0, 0}});
+    runAnim.addFrame({{45, 204, 50, 48}, 5.5f, {0, 0}});
+    runAnim.addFrame({{92, 204, 50, 48}, 5.5f, {0, 0}});
+    runAnim.addFrame({{138, 204, 50, 48}, 5.5f, {0, 0}});
+    runAnim.addFrame({{0, 204, 50, 48}, 5.5f, {0, 0}});
+    runAnim.addFrame({{184, 204, 50, 48}, 5.5f, {0, 0}});
+    runAnim.addFrame({{230, 204, 50, 48}, 5.5f, {0, 0}});
+    runAnim.addFrame({{276, 204, 50, 48}, 5.5f, {0, 0}});
     runAnim.setLoop(true);
 
     Animation idleAnim;
-    idleAnim.addFrame({{0, 0, 25, 26}, 0.2f, {0, 0}});
+    idleAnim.addFrame({{0, 0, 40, 48}, 7.5f, {0, 0}});
+    idleAnim.addFrame({{45, 0, 40, 48}, 7.5f, {0, 0}});
+    idleAnim.addFrame({{91, 0, 40, 48}, 7.5f, {0, 0}});
     idleAnim.setLoop(true);
+
+    Animation punchAnim;
+    punchAnim.addFrame({{0, 102, 29, 45}, 0.1f, {0, 0}});
+    punchAnim.addFrame({{46, 102, 29, 45}, 0.1f, {0, 0}});
+    punchAnim.addFrame({{0, 102, 29, 45}, 0.1f, {0, 0}});
+    punchAnim.setLoop(false);
+
+    Animation strongPunchAnim;
+    strongPunchAnim.addFrame({{0, 153, 29, 45}, 0.1f, {0, 0}});
+    strongPunchAnim.addFrame({{46, 153, 29, 45}, 0.1f, {0, 0}});
+    strongPunchAnim.setLoop(false);
+
+    Animation jumpAnim;
+    jumpAnim.addFrame({{0, 50, 29, 45}, 0.1f, {0, 0}});
+    jumpAnim.addFrame({{46, 51, 29, 45}, 0.1f, {0, 0}});
+    jumpAnim.setLoop(false);
+
 
     mSprite.addAnimation("run", std::move(runAnim));
     mSprite.addAnimation("idle", std::move(idleAnim));
+    mSprite.addAnimation("punch", std::move(punchAnim));
+    mSprite.addAnimation("strong_punch", std::move(strongPunchAnim));
+    mSprite.addAnimation("jump", std::move(jumpAnim));
     mSprite.play("idle");
 }
 
@@ -76,9 +103,14 @@ void Player::update(float deltaTime) {
     // Controle de animações
     if(velocity.x != 0.0f) {
         mSprite.play("run");
-    } else {
+    }  
+    else if(!isOnGround()) {
+        mSprite.play("jump");
+    } 
+    else {
         mSprite.play("idle");
     }
+   
     
     mSprite.update(deltaTime);
 }
