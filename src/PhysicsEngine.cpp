27b0cd79b72bb39bf71e8fe25fa421cc624ea7da
netorag <renatoplatz11@outlook.com>
2025-03-30 19:08:50 -0300

    ajuestei a colisão da plataforma vazada por baixo

--- src/PhysicsEngine.cpp
+++ src/PhysicsEngine.cpp
@@ -68,15 +68,10 @@ namespace BRTC
                     const Vector platformPos = platform.getPosition();
                     const Vector platformSize = platform.getSize();
                     
-                    if (velocity.y > 0) {
+                    if (velocity.y >= 0) {
                         position.y = platformPos.y - size.y;
                         velocity.y = 0;
                         dynamicObject.setOnGround(true);
-                    //    std::cout << "isOnGround: " << dynamicObject.isOnGround() << std::endl;
-                    } else if (velocity.y < 0) {
-                        position.y = platformPos.y - platformSize.y;
-                        velocity.y = 0;
-
                     }
                 }
             }
